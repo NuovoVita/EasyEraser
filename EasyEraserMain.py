c7b037872242278810a73ce99b15b01e0c485919
root <root@qq.com>
2022-08-31 10:38:53 +0800

    Optimize code

--- EasyEraserMain.py
+++ EasyEraserMain.py
@@ -4,48 +4,60 @@ import sys
 from pathlib import Path
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QDialog, QFileDialog, QMessageBox, QTableWidgetItem, QPushButton, QHeaderView
+from PyQt5.QtWidgets import QApplication, QDialog, QFileDialog, QMessageBox, QTableWidgetItem, QHeaderView
+from timeout_decorator import timeout, timeout_decorator
 
 from EasyEraser import Ui_Dialog
 
 
 class EasyEraserMainDialog(QDialog):
     root_path = '/'
+    max_timeout = 30
 
     def __init__(self, parent=None):
         super(QDialog, self).__init__(parent)
-        self.setWindowFlags(Qt.WindowMinMaxButtonsHint | Qt.WindowCloseButtonHint)
+        self.setWindowFlags(Qt.WindowMinimizeButtonHint | Qt.WindowCloseButtonHint)
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
-        self.ui.dir_text.setPlainText('/opt')
         self.ui.file_table.setRowCount(10)
         self.ui.file_table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
         # self.ui.file_table.setColumnWidth(0, 650)
         # self.ui.file_table.setColumnWidth(1, 200)
         # self.ui.file_table.horizontalHeader().setSectionResizeMode(0, QHeaderView.ResizeToContents)
 
+    @timeout(max_timeout)
+    def _find_all_files(self, find_dir, find_file):
+        find_file_lst = []
+        for parent, _, files in os.walk(find_dir):
+            for file_ in files:
+                if find_file in file_:
+                    _file_path = QTableWidgetItem(str(Path(parent).joinpath(file_)))
+                    find_file_lst.append(_file_path)
+                    self.ui.file_table.setItem(len(find_file_lst) - 1, 0, _file_path)
+                    # _rm_btn = QPushButton('删除')
+                    # _rm_btn.setDown(True)
+                    # self.ui.file_table.setCellWidget(counter, 1, _rm_btn)
+        return find_file_lst
+
     def search_file(self):
         search_dir = self.ui.dir_text.toPlainText()
+        if not search_dir:
+            QMessageBox.information(self, '搜索结果提示框', '请输入文件路径', QMessageBox.Yes)
+            return
+
         search_file = self.ui.file_text.toPlainText()
         if not search_file:
             QMessageBox.information(self, '搜索结果提示框', '请输入文件名', QMessageBox.Yes)
             return
 
-        has_file_flag = False
-        counter = 0
-        for parent, _, files in os.walk(search_dir):
-            for file_ in files:
-                if search_file in file_:
-                    has_file_flag = True
-                    _file_path = QTableWidgetItem(str(Path(parent).joinpath(file_)))
-                    self.ui.file_table.setItem(counter, 0, _file_path)
-                    _rm_btn = QPushButton('删除')
-                    _rm_btn.setDown(True)
-                    self.ui.file_table.setCellWidget(counter, 1, _rm_btn)
-                    counter += 1
-
-        if not has_file_flag:
-            msg = '在目录{}下，未找到{}文件'.format(search_dir, search_file)
+        try:
+            result = self._find_all_files(search_dir, search_file)
+            if not result:
+                msg = '在目录“{}”下未找到“{}”文件'.format(search_dir, search_file)
+                QMessageBox.information(self, '搜索结果提示框', msg, QMessageBox.Yes)
+                return
+        except timeout_decorator.TimeoutError:
+            msg = '在目录“{}”下搜索文件“{}”超时，请重新搜索'.format(search_dir, search_file)
             QMessageBox.information(self, '搜索结果提示框', msg, QMessageBox.Yes)
             return
 
@@ -71,8 +83,8 @@ class EasyEraserMainDialog(QDialog):
             return
 
         _file_path = cell.text()
-        msg = '确定要删除{}吗？'.format(_file_path)
-        result = QMessageBox.question(self, '删除确认框', msg, QMessageBox.Yes | QMessageBox.No)
+        msg = '确定要擦除“{}”吗？'.format(_file_path)
+        result = QMessageBox.question(self, '擦除确认框', msg, QMessageBox.Yes | QMessageBox.No)
         if result == QMessageBox.Yes:
             Path(_file_path).unlink(missing_ok=True)
             self.ui.file_table.removeRow(row_num)
--- example.py
+++ example.py
@@ -1,6 +1,8 @@
 import sys
+import time
 
 from PyQt5 import QtCore, QtWidgets
+from timeout_decorator import timeout, timeout_decorator
 
 
 class WidgetGallery(QtWidgets.QDialog):
@@ -10,7 +12,7 @@ class WidgetGallery(QtWidgets.QDialog):
         col_1 = QtWidgets.QTableWidgetItem("first_col")
         col_2 = QtWidgets.QTableWidgetItem("second_col")
         deleteButton = QtWidgets.QPushButton("delete_this_row")
-        deleteButton.clicked.connect(self.deleteClicked)
+        deleteButton.clicked.connect(self.delete_clicked)
         self.table.setItem(0, 0, col_1)
         self.table.setItem(0, 1, col_2)
         self.table.setCellWidget(0, 2, deleteButton)
@@ -18,7 +20,7 @@ class WidgetGallery(QtWidgets.QDialog):
         col_1 = QtWidgets.QTableWidgetItem("2first_col")
         col_2 = QtWidgets.QTableWidgetItem("2second_col")
         deleteButton = QtWidgets.QPushButton("2delete_this_row")
-        deleteButton.clicked.connect(self.deleteClicked)
+        deleteButton.clicked.connect(self.delete_clicked)
         self.table.setItem(1, 0, col_1)
         self.table.setItem(1, 1, col_2)
         self.table.setCellWidget(1, 2, deleteButton)
@@ -26,15 +28,41 @@ class WidgetGallery(QtWidgets.QDialog):
         self.mainLayout.addWidget(self.table)
 
     @QtCore.pyqtSlot()
-    def deleteClicked(self):
+    def delete_clicked(self):
         button = self.sender()
         if button:
             row = self.table.indexAt(button.pos()).row()
             self.table.removeRow(row)
 
 
+class WidgetTableExample(object):
+    @classmethod
+    def run(cls):
+        app = QtWidgets.QApplication(sys.argv)
+        w = WidgetGallery()
+        w.show()
+        sys.exit(app.exec_())
+
+
+class TimeoutExample(object):
+    time_out = 5
+
+    @classmethod
+    @timeout(time_out)
+    def worker(cls):
+        print('Start')
+        for index in range(1, 10):
+            time.sleep(1)
+            print('{} seconds have passed'.format(index))
+
+    @classmethod
+    def run(cls):
+        try:
+            cls.worker()
+        except timeout_decorator.TimeoutError:
+            print('Run TIMEOUT!!!')
+
+
 if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    w = WidgetGallery()
-    w.show()
-    sys.exit(app.exec_())
+    # WidgetTableExample.run()
+    TimeoutExample.run()
